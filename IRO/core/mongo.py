60ccdad321cd286356526e2599cc0f9417ad63dd
Pikachu <116916755+Iro09@users.noreply.github.com>
2022-12-02 07:48:10 +0000

    Update mongo.py

--- IRO/core/mongo.py
+++ IRO/core/mongo.py
@@ -11,10 +11,10 @@ TEMP_MONGODB = "mongodb+srv://anonxmusic:anonxmusic@anonx.9v4i8.mongodb.net/?ret
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "Your bot will work on @anonymous_was_bot mongo database..."
+        "Your bot will work on @pikk_u mongo database..."
     )
     temp_client = Client(
-        "AnonX",
+        "Iro",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -30,5 +30,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    pymongodb = _mongo_sync_.Anon
+    mongodb = _mongo_async_.Iro
+    pymongodb = _mongo_sync_.Iro
