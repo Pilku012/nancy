9966b118fc376f8a629349f6cfd24f1f6bf83dab
Pikachu <116916755+Iro09@users.noreply.github.com>
2022-12-02 07:37:50 +0000

    Add files via upload

---
+++ IRO/__init__.py
@@ -0,0 +1,41 @@
+from Iro.core.bot import IroBot
+from Iro.core.dir import dirr
+from Iro.core.git import git
+from Iro.core.userbot import Userbot
+from Iro.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = IroBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
---
+++ IRO/plugins/Robot/stop.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from Iro import app
+from Iro.core.call import Iro
+from Iro.utils.database import set_loop
+from Iro.utils.decorators import AdminRightsCheck
+
+# Commands
+STOP_COMMAND = get_command("STOP_COMMAND")
+
+
+@app.on_message(
+    filters.command(STOP_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Iro.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_9"].format(message.from_user.mention)
+    )
